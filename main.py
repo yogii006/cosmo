b8301bc66a645a74fcfd2c93afb47975ad39679b
Yogesh Yadav <128319693+yogii006@users.noreply.github.com>
2024-12-03 18:10:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ from database import db
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello, Render!"}
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT is not set
+    uvicorn.run(app, host="0.0.0.0", port=port)
+
 class AddressModel(BaseModel):
     city: str
     country: str
