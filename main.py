bd8248f4fa1008c926ffb6d6531b7cc75235aeab
Yogesh Yadav <128319693+yogii006@users.noreply.github.com>
2024-12-03 18:23:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def read_root():
     return {"message": "Hello, Render!"}
 
 if __name__ == "__main__":
+    # Bind to the port specified in the environment variable
     port = int(os.getenv("PORT", 8000))  # Default to 8000 if PORT is not set
     uvicorn.run(app, host="0.0.0.0", port=port)
 
